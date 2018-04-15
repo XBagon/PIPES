534c22b01fda6149328f5ec803456b0145ef6c97
XBagon <xbagon@outlook.de>
2018-04-15 17:42:04 +0200

    checks 4 sides instead of 8; added DUP,IN[WIP],OUT,TO_CHAR operations

--- main.js
+++ main.js
@@ -9,16 +9,21 @@ const color_path = new color(0,0,0);
 const color_blockade = new color(127,127,127);
 
 const color_push = new color(0,0,255);
+const color_dup = new color(127,127,255);
 const color_add = new color(38,127,0);
 const color_sub = new color(87,0,124);
 const color_mul = new color(124,24,0);
 const color_div = new color(0,99,124);
 
-const operation_push = 1;
-const operation_add = 2;
-const operation_sub = 3;
-const operation_mul = 4;
-const operation_div = 5;
+const color_in = new color(255,216,0);
+const color_out = new color(255,106,0);
+const color_to_char = new color(255,206,127);
+
+// const operation_push = 1;
+// const operation_add = 2;
+// const operation_sub = 3;
+// const operation_mul = 4;
+// const operation_div = 5;
 
 function color(R, G, B){
     this.R = R;
@@ -38,7 +43,9 @@ function colorFromPixel(data, index){
     return new color(data[index], data[index+1], data[index+2]);
 }
 
-commander.parse(process.argv);
+commander.
+    option("-d, --debug","Debug mode").
+    parse(process.argv);
 
 
 console.log(commander.args[0]);
@@ -77,21 +84,19 @@ function findEntry(img){
     return null;
 }
 
-//TODO: 4 neighbors instead of 8
-//TODO: maybe straight, right then left for cooler looks
-var neighborTiles = [{x:-1,y:-1},{x:-1,y:0},{x:-1,y:1},{x:0,y:1},{x:1,y:1},{x:1,y:0},{x:1,y:-1},{x:0,y:-1},{x:-1,y:-1},{x:-1,y:0},{x:-1,y:1},{x:0,y:1},{x:1,y:1},{x:1,y:0},{x:1,y:-1},{x:0,y:-1}];
 
+var neighborTiles  = [{x:-1,y:0},{x:0,y:1},{x:1,y:0},{x:0,y:-1},{x:-1,y:0},{x:0,y:1},{x:1,y:0},{x:0,y:-1}];
 
 
 function walker(img) {
     this.img = img;
     this.current;
-    this.lastNeighbor = 0;
+    this.lastDirection = 0;
     this.finished = false;
     this.stack = [];
-    this.operation = 0;
+    this.push = false;
     this.start = function(){
-        this.current = findEntry(img);
+        this.current = findEntry(this.img);
         if(this.current == null) console.error("No entry point found.");
 
         console.log("ENTRY");
@@ -101,87 +106,94 @@ function walker(img) {
     this.nextTile = function() {
         var blockade = false;
         var next;
-        var offset = (this.lastNeighbor + 1) % 8;
-        for (let i = offset; i < 8 + offset; i++) {
-            var neighbor = neighborTiles[i];
+        var direction;
+        if(this.lastDirection > 5) this.lastDirection -= 4;
+        else if(this.lastDirection < 1) this.lastDirection += 4;
+        for (var i of [0,-1,1,2]){
+            var neighbor = neighborTiles[i+this.lastDirection];
             var pos = {x: this.current.x + neighbor.x, y:this.current.y + neighbor.y};
-            var idx = (img.width * pos.y + pos.x) << 2;
-            var pixel = colorFromPixel(img.data, idx);
+            var idx = (this.img.width * pos.y + pos.x) << 2;
+            var pixel = colorFromPixel(this.img.data, idx);
             if (!pixel.equals(color_background)) {
-                if(blockade && i==7+offset) break;
+                if(blockade && i==2) break;
                 if (pixel.equals(color_blockade)) {
                     if(!blockade) {
                         next = pos;
-                        this.lastNeighbor = i + 4;
+                        direction = i+this.lastDirection;
                         blockade = true;
                     }
                 } else {
-                    next = pos;
-                    this.lastNeighbor = i+4;
+                    this.current = pos;
+                    this.lastDirection = i+this.lastDirection;
+                    if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack);
                     this.onColor(pixel);
-                    break;
+                    return;
                 }
             }
         }
-
         this.current = next;
+        this.lastDirection = direction;
+        if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack);
     };
     this.onColor = function(color){
         // if parameters
-        if(this.operation == 0) {
+        if (this.push) {
+
+            if (commander.debug) console.log("NUMBER: " + color.toNumber());
+            this.stack.push(color.toNumber());
+
+            this.push = false;
+        }
+        else {
             if (color.equals(color_path)) return;
 
-            console.log(this.current + this.stack);
 
             if (color.equals(color_push)) {
-                console.log("PUSH");
-                this.operation = operation_push;
+                if (commander.debug) console.log("PUSH");
+                this.push = true;
+            }
+            else if (color.equals(color_dup)) {
+                if (commander.debug) console.log("DUP");
+                this.stack.push(this.stack[this.stack.length-1]);
             }
 
 
             else if (color.equals(color_add)) {
-                console.log("ADD");
+                if (commander.debug) console.log("ADD");
                 this.stack.push(this.stack.pop() + this.stack.pop());
             }
             else if (color.equals(color_sub)) {
-                console.log("SUB");
+                if (commander.debug) console.log("SUB");
                 this.stack.push(-this.stack.pop() + this.stack.pop());
             }
             else if (color.equals(color_mul)) {
-                console.log("MUL");
+                if (commander.debug) console.log("MUL");
                 this.stack.push(this.stack.pop() * this.stack.pop());
             }
             else if (color.equals(color_div)) {
-                console.log("DIV");
-                this.stack.push(1/this.stack.pop() * this.stack.pop());
+                if (commander.debug) console.log("DIV");
+                this.stack.push(1 / this.stack.pop() * this.stack.pop());
             }
+
+            else if (color.equals(color_in)) {
+                if (commander.debug) console.log("IN");
+                console.error("IN is not yet implemented.")
+            }
+            else if (color.equals(color_out)) {
+                if (commander.debug) console.log("OUT");
+                console.log(this.stack.pop());
+            }
+            else if (color.equals(color_to_char)) {
+                if (commander.debug) console.log("TO_CHAR");
+                this.stack.push(String.fromCharCode(this.stack.pop()));
+            }
+
+
 
             else if (color.equals(color_exit)) {
-                console.log("EXIT");
+                if (commander.debug) console.log("EXIT");
                 this.finished = true;
             }
-        }
-        else{
-            switch(this.operation){
-                case operation_push:
-                    console.log("NUMBER: " + color.toNumber());
-                    this.stack.push(color.toNumber());
-                    break;
-                case operation_add:
-
-                    break;
-                case operation_sub:
-
-                    break;
-                case operation_mul:
-
-                    break;
-                case operation_div:
-
-                    break;
-            }
-            //console.log("color: { R: " + color.R + ", G: " + color.G + ", B: " + color.B + " }; number: " + color.toNumber() + "; operation: " + this.operation + "; stack: " + this.stack);
-            this.operation = 0;
         }
     };
 }
