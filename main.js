a94fc6c725d18b36c1dcaaedc441bbab038e19b8
XBagon <xbagon@outlook.de>
2018-04-15 23:16:41 +0200

    added CMP, SWP, STM, MTS and new examples

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
 const fs = require("fs");
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 const commander = require("commander");
 const PNG = require("pngjs").PNG;
-
-const color_entry = new color(0,255,0);
-const color_exit = new color(255,0,0);
-const color_background = new color(255,255,255);
-const color_path = new color(0,0,0);
-const color_blockade = new color(127,127,127);
-
-const color_push = new color(0,0,255);
-const color_dup = new color(127,127,255);
-const color_add = new color(38,127,0);
-const color_sub = new color(87,0,124);
-const color_mul = new color(124,24,0);
-const color_div = new color(0,99,124);
-
-const color_in = new color(255,216,0);
-const color_out = new color(255,106,0);
-const color_to_char = new color(255,206,127);
-
-// const operation_push = 1;
-// const operation_add = 2;
-// const operation_sub = 3;
-// const operation_mul = 4;
-// const operation_div = 5;
 
 function color(R, G, B){
     this.R = R;
@@ -43,9 +25,12 @@ function colorFromPixel(data, index){
     return new color(data[index], data[index+1], data[index+2]);
 }
 
-commander.
-    option("-d, --debug","Debug mode").
-    parse(process.argv);
+commander
+    .option("-d, --debug","Debug mode")
+    .option("-s, --strict","Strict mode")
+    .option("-i, --input [args]","Input arguments")
+    .option("-p, --stepwise","Stepwise execution")
+    .parse(process.argv);
 
 
 console.log(commander.args[0]);
@@ -68,21 +53,119 @@ if(commander.args[0]) {
 
 function interpret(img){
     var w = new walker(img);
-    w.start();
-}
-
-function findEntry(img){
-    for (let y = 0; y < img.height; y++) {
-        for (let x = 0; x < img.width; x++) {
-            var idx = (img.width * y + x) << 2;
-            var pixel = colorFromPixel(img.data, idx);
-            if(pixel.equals(color_entry)){
-                return {x,y};
-            }
+    if(commander.input) {
+        for (let i = 0; i < commander.input.length; i++) {
+            w.stack.unshift(commander.input.charCodeAt(i));
         }
     }
-    return null;
+    w.colormandos.push(
+        {
+            name : "DUP" ,
+            color : new color(127,127,255),
+            func : function() {
+                this.stack.push(this.stack[this.stack.length-1]);
+            }
+        },
+        {
+            name : "SWP" ,
+            color : new color(178,0,255),
+            func : function() {
+                var a = this.stack.pop();
+                var b = this.stack.pop();
+                this.stack.push(a);
+                this.stack.push(b);
+            }
+        },
+        {
+            name : "ADD" ,
+            color : new color(38,127,0),
+            func : function() {
+                this.stack.push(this.stack.pop() + this.stack.pop());
+            }
+        },
+        {
+            name : "SUB" ,
+            color : new color(87,0,124),
+            func : function() {
+                this.stack.push(-this.stack.pop() + this.stack.pop());
+            }
+        },
+        {
+            name : "MUL" ,
+            color : new color(124,24,0),
+            func : function() {
+                this.stack.push(this.stack.pop() * this.stack.pop());
+            }
+        },
+        {
+            name : "DIV" ,
+            color : new color(0,99,124),
+            func : function() {
+                this.stack.push(1/this.stack.pop() * this.stack.pop());
+            }
+        },
+        {
+            name : "CMP" ,
+            color : new color(168,97,11),
+            func : function() {
+                var diff = this.stack[this.stack.length-2] - this.stack[this.stack.length-1];
+                if(diff > 0){
+                    this.lastDirection--;
+                }else if(diff < 0){
+                    this.lastDirection++;
+                }
+            }
+        },
+        {
+            name : "STM" ,
+            color : new color(0,255,144),
+            func : function() {
+                this.memory[this.stack.pop()] = this.stack[this.stack.length-1];
+            }
+        },
+        {
+            name : "MTS" ,
+            color : new color(0,225,255),
+            func : function() {
+                this.stack.push(this.memory[this.stack.pop()]);
+            }
+        },
+        {
+            name : "IN" ,
+            color : new color(255,216,0),
+            func : function() {
+                console.error("IN is not yet implemented.")
+            }
+        },
+        {
+            name : "OUT" ,
+            color : new color(255,106,0),
+            func : function() {
+                console.log(this.stack.pop());
+            }
+        },
+        {
+            name : "CHAR_OUT" ,
+            color : new color(255,206,127),
+            func : function() {
+                process.stdout.write(String.fromCharCode(this.stack.pop()));
+            }
+        },
+    );
+
+    w.stepwise = commander.stepwise;
+    w.start();
+
+
+    // if(commander.stepwise){
+    //     rl.question("", () => {
+    //         w.nextTile();
+    //
+    //     });
+    // }
 }
+
+
 
 
 var neighborTiles  = [{x:-1,y:0},{x:0,y:1},{x:1,y:0},{x:0,y:-1},{x:-1,y:0},{x:0,y:1},{x:1,y:0},{x:0,y:-1}];
@@ -93,15 +176,39 @@ function walker(img) {
     this.current;
     this.lastDirection = 0;
     this.finished = false;
+    this.stepwise = false;
     this.stack = [];
+    this.memory = [];
     this.push = false;
+    this.colormandos = [];
+    this.color_entry = new color(0,255,0);
+    this.color_exit = new color(255,0,0);
+    this.color_background = new color(255,255,255);
+    this.color_path = new color(0,0,0);
+    this.color_blockade = new color(127,127,127);
+    this.color_push = new color(0,0,255);
+    this.color_pop = new color(255,255,0);
     this.start = function(){
-        this.current = findEntry(this.img);
+        this.current = this.findEntry(this.img);
         if(this.current == null) console.error("No entry point found.");
 
         console.log("ENTRY");
 
-        while(!this.finished) this.nextTile();
+        if(!this.stepwise){
+            while (!this.finished) this.nextTile();
+        }
+    };
+    this.findEntry = function(img){
+        for (let y = 0; y < img.height; y++) {
+            for (let x = 0; x < img.width; x++) {
+                var idx = (img.width * y + x) << 2;
+                var pixel = colorFromPixel(img.data, idx);
+                if(pixel.equals(this.color_entry)){
+                    return {x,y};
+                }
+            }
+        }
+        return null;
     };
     this.nextTile = function() {
         var blockade = false;
@@ -114,9 +221,9 @@ function walker(img) {
             var pos = {x: this.current.x + neighbor.x, y:this.current.y + neighbor.y};
             var idx = (this.img.width * pos.y + pos.x) << 2;
             var pixel = colorFromPixel(this.img.data, idx);
-            if (!pixel.equals(color_background)) {
+            if (!pixel.equals(this.color_background)) {
                 if(blockade && i==2) break;
-                if (pixel.equals(color_blockade)) {
+                if (pixel.equals(this.color_blockade)) {
                     if(!blockade) {
                         next = pos;
                         direction = i+this.lastDirection;
@@ -125,7 +232,7 @@ function walker(img) {
                 } else {
                     this.current = pos;
                     this.lastDirection = i+this.lastDirection;
-                    if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack);
+                    if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack + " MEM: " + this.memory);
                     this.onColor(pixel);
                     return;
                 }
@@ -133,7 +240,7 @@ function walker(img) {
         }
         this.current = next;
         this.lastDirection = direction;
-        if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack);
+        if(commander.debug)console.log("[" + this.current.x + " | " + this.current.y + "] STACK: " + this.stack + " MEM: " + this.memory);
     };
     this.onColor = function(color){
         // if parameters
@@ -145,55 +252,42 @@ function walker(img) {
             this.push = false;
         }
         else {
-            if (color.equals(color_path)) return;
+            if (color.equals(this.color_path)) return;
 
 
-            if (color.equals(color_push)) {
+            if (color.equals(this.color_push)) {
                 if (commander.debug) console.log("PUSH");
                 this.push = true;
+                return;
             }
-            else if (color.equals(color_dup)) {
-                if (commander.debug) console.log("DUP");
-                this.stack.push(this.stack[this.stack.length-1]);
-            }
-
-
-            else if (color.equals(color_add)) {
-                if (commander.debug) console.log("ADD");
-                this.stack.push(this.stack.pop() + this.stack.pop());
-            }
-            else if (color.equals(color_sub)) {
-                if (commander.debug) console.log("SUB");
-                this.stack.push(-this.stack.pop() + this.stack.pop());
-            }
-            else if (color.equals(color_mul)) {
-                if (commander.debug) console.log("MUL");
-                this.stack.push(this.stack.pop() * this.stack.pop());
-            }
-            else if (color.equals(color_div)) {
-                if (commander.debug) console.log("DIV");
-                this.stack.push(1 / this.stack.pop() * this.stack.pop());
-            }
-
-            else if (color.equals(color_in)) {
-                if (commander.debug) console.log("IN");
-                console.error("IN is not yet implemented.")
-            }
-            else if (color.equals(color_out)) {
-                if (commander.debug) console.log("OUT");
-                console.log(this.stack.pop());
-            }
-            else if (color.equals(color_to_char)) {
-                if (commander.debug) console.log("TO_CHAR");
-                this.stack.push(String.fromCharCode(this.stack.pop()));
+            if (color.equals(this.color_pop)) {
+                if (commander.debug) console.log("POP");
+                this.stack.pop();
+                return;
             }
 
 
+            for (cm of this.colormandos){
+                if(color.equals(cm.color)){
+                    if(commander.debug) console.log(cm.name);
+                    cm.func.call(this);
+                    return;
+                }
+            }
 
-            else if (color.equals(color_exit)) {
+            if (color.equals(this.color_exit)) {
                 if (commander.debug) console.log("EXIT");
                 this.finished = true;
             }
+            else{
+                if(commander.strict) console.error("Unknown color found.");
+            }
         }
     };
+}
+
+
+module.exports = {
+    walker,
+    color
 }
